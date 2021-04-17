d0f18d3598c695e9b17eae6bacfbc1ccb4c92e67
SlawomirJakubek <git@jakubek.co.uk>
2021-04-17 01:29:54 +0100

    HTML and CSS for 'about' and 'projects' sections

--- index.php
+++ index.php
@@ -22,17 +22,21 @@
                 </ul>
                 <button id="menuBtn" onclick="toggleMenu()">&#9776;</button>
             </nav>
-            <section id="banner" class="banner-loading">
-                <p id="banner-name">SLAWOMIR</p>
-                <div id="banner-animation"></div>
-                <script>$('#banner-animation').append(new Sphere(280));</script>
-                <p id="banner-surname">JAKUBEK</p>
+            <section id="banner" style="overflow-y: scroll">
+                <div id="banner-container">
+                    <p id="banner-name">SLAWOMIR</p>
+                    <div id="banner-animation"></div>
+                    <script>$('#banner-animation').append(new Sphere(280));</script>
+                    <p id="banner-surname">JAKUBEK</p>
+                </div>
             </section>
         </header>
 
         <main>
             <section id="about">
                 <h2>ABOUT</h2>
+                <img src="./img/author.jpg" alt="Image of Slawomir Jakubek, author of this portfolio" />
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </section>
 
             <section id="projects">
--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,10 @@ let isMenuOpen = false;
 let scrollNavBar = false;
 let windowScrollHistory = [0, 0];
 let windowLastScroll;
+const style = getComputedStyle(document.documentElement);
+const menuHeight = parseInt(style.getPropertyValue('--menuHeight').replace('px', ''));
+const menuButtonHeight = parseInt(style.getPropertyValue('--menuButtonHeight').replace('px', ''));
+const menuStandbyTop = (menuHeight - menuButtonHeight) * -1;
 
 function toggleMenu(goto){
     
@@ -15,8 +19,9 @@ function toggleMenu(goto){
         if(typeof goto === 'undefined'){
             $(window).scrollTop(windowLastScroll);
         }else{
-            window.location.href = `#${goto}`;
-            $(window).scrollTop($(window).scrollTop() - 62);
+            //window.location.href = `#${goto}`;
+            window.scrollTo(0, document.getElementById(goto).getBoundingClientRect().top);
+            $(window).scrollTop($(window).scrollTop() - menuButtonHeight);
         }
         
     }else{
@@ -26,7 +31,7 @@ function toggleMenu(goto){
 
         $('#banner').addClass('disabled');
         $(window).scrollTop(0);
-        $('body').addClass('noScroll');
+        $('html, body').addClass('noScroll');
     }
     isMenuOpen = !isMenuOpen;
     $("nav").css('top', '');
@@ -41,79 +46,74 @@ $(document).ready(()=>{
         if(isMenuOpen){
             $("nav").css('top', '0px');
         }else{
-            $("nav").css('top', '-154px');
+            $("nav").css('top', `${menuStandbyTop}px`);
         }
         console.log(e);
     });
 });
 
 $(window).on('load', () => {
+
     console.log('loaded');
-    //on logo loaded
+    //on page loaded
     window.setTimeout(()=>{
+        $('#banner').css('overflow-y', '');
+        $('html, body').removeClass('noScroll');
 
-        //on page loaded
-        window.setTimeout(()=>{
-            $('#banner').removeClass('banner-loading');
-            $('#banner').addClass('banner-loaded');
-            $('html, body').removeClass('noScroll');
+        $(window).scroll(e => {
 
-            $(window).scroll(e => {
+            
+            windowScrollHistory.push($(window).scrollTop());
+    
+            if(windowScrollHistory.length > 2){
+                windowScrollHistory.shift();
+            }
 
-                
-                windowScrollHistory.push($(window).scrollTop());
-        
-                if(windowScrollHistory.length > 2){
-                    windowScrollHistory.shift();
-                }
-
-                let isScrollingDown = windowScrollHistory[0] < windowScrollHistory[1];
+            let isScrollingDown = windowScrollHistory[0] < windowScrollHistory[1];
 
 
-                //show or hide nav bar on scroll
-                if(!isMenuOpen && scrollNavBar){
+            //show or hide nav bar on scroll
+            if(!isMenuOpen && scrollNavBar){
 
-                    let navTop = parseInt($("nav").css('top').replace('px', ''));
-                    let newTop;
-                    if(isScrollingDown){
-                        
-                        if(navTop <= -220){
-                            newTop = -220;
-                        }else{
-                            newTop = navTop - 2;
-                        }
-
+                let navTop = parseInt($("nav").css('top').replace('px', ''));
+                let newTop;
+                if(isScrollingDown){
+                    
+                    if(navTop <= (menuHeight + 5) * -1){
+                        newTop = (menuHeight + 5) * -1;
                     }else{
-
-                        if(navTop >= -154 || windowScrollHistory[1] == 0){
-                            newTop = -154;
-                        }else{
-                            newTop = navTop + 4;
-                        }
-
+                        newTop = navTop - 2;
                     }
-                    $("nav").css('top', `${newTop}px`);
+
+                }else{
+
+                    if(navTop >= menuStandbyTop || windowScrollHistory[1] == 0){
+                        newTop = menuStandbyTop;
+                    }else{
+                        newTop = navTop + 4;
+                    }
+
                 }
+                $("nav").css('top', `${newTop}px`);
+            }
 
-                console.log(windowScrollHistory);
-            });
-            
-            $(window).on('click', e => {
-                console.log(e);
-            });
+            console.log(windowScrollHistory);
+        });
+        
+        $(window).on('click', e => {
+            console.log(e);
+        });
 
-            //display menu
-            $('nav').css('transition', 'top .5s');
-            $('nav').css('top', '-154px');
-            window.scrollTo(0, document.getElementById('about').getBoundingClientRect().top - 62);
-            
-            window.setTimeout(()=>{
-                $('html').css('scroll-behavior', '');
-                $('nav').css('transition', '');
-                scrollNavBar = true;
-            }, 1000);
-
+        //display menu
+        $('nav').css('transition', 'top .5s');
+        $('nav').css('top', `${menuStandbyTop}px`);
+        window.scrollTo(0, document.getElementById('about').getBoundingClientRect().top - menuButtonHeight);
+        
+        window.setTimeout(()=>{
+            $('html').css('scroll-behavior', '');
+            $('nav').css('transition', '');
+            scrollNavBar = true;
         }, 1000);
-    }, 1000);
 
+    }, 0000);
 });
